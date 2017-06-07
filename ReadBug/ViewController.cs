a49e632163e128ccd855fd7631be1ab5727a3159
David Ingham <david.ingham@cse-healthcare.com>
2017-06-07 08:25:05 +0100

    Example on UIThread and it's all working.

---
+++ ReadBug/ViewController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using UIKit;
+
+namespace ReadBug
+{
+    public partial class ViewController : UIViewController
+    {
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            this.AddressBox.Text = "http://hsw10473-7vm.cse-servelec.com/rio/storeandforward/assessmentsresources.txt";
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        partial void FetchDocumentTouched(Foundation.NSObject sender)
+        {
+            this.DisplayOutput("Downloading ...");
+            this.DownloadAndDisplayFile(this.AddressBox.Text);
+        }
+
+        private void DisplayOutput(string output)
+        {
+            this.InvokeOnMainThread(() => { this.OutputArea.Text = output; });
+        }
+
+        private void DownloadAndDisplayFile(string urlToDownload)
+        {
+			var url = new Uri(urlToDownload);
+			var request = (HttpWebRequest)WebRequest.Create(url);
+			var response = request.GetResponse();
+			var stringBuilder = new StringBuilder();
+
+			using (var stream = response.GetResponseStream())
+			{
+				if (stream != null)
+				{
+					const int BufferSize = 256;
+					var encoding = Encoding.ASCII;
+					using (var streamReader = new StreamReader(stream, encoding))
+					{
+						var buffer = new char[BufferSize];
+						var count = streamReader.Read(buffer, 0, BufferSize);
+						while (count > 0)
+						{
+							stringBuilder.Append(new string(buffer, 0, count));
+							count = streamReader.Read(buffer, 0, BufferSize);
+						}
+
+						streamReader.Close();
+						stream.Close();
+						response.Close();
+					}
+				}
+			}
+
+            this.DisplayOutput(stringBuilder.ToString());
+        }
+    }
+}
---
+++ ReadBug/ViewController.designer.cs
@@ -0,0 +1,45 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace ReadBug
+{
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+		[Outlet]
+		UIKit.UITextField AddressBox { get; set; }
+
+		[Outlet]
+		UIKit.UIButton FetchDocumentButton { get; set; }
+
+		[Outlet]
+		UIKit.UITextView OutputArea { get; set; }
+
+		[Action ("FetchDocumentTouched:")]
+		partial void FetchDocumentTouched (Foundation.NSObject sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (AddressBox != null) {
+				AddressBox.Dispose ();
+				AddressBox = null;
+			}
+
+			if (FetchDocumentButton != null) {
+				FetchDocumentButton.Dispose ();
+				FetchDocumentButton = null;
+			}
+
+			if (OutputArea != null) {
+				OutputArea.Dispose ();
+				OutputArea = null;
+			}
+		}
+	}
+}
